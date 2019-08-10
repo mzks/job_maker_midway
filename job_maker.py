209235c29fb5c431ef1115458754c8d58ab164b9
Keita Mizukoshi <mzks@stu.kobe-u.ac.jp>
2019-08-09 23:24:17 -0500

    Chenge directory to store generated scripts

--- job_maker.py
+++ job_maker.py
@@ -15,8 +15,8 @@ def make_macro(seed):
 	loaded_macro_fname = workdir+'/macros/XENONnT/' + run_macro_name + '.mac'
 	output_dirname = 'macro'
 	fin = open(loaded_macro_fname, mode='r')
-	os.makedirs('./made/'+run_macro_name+'/'+output_dirname, exist_ok=True)
-	fout = open('./made/'+run_macro_name+'/'+output_dirname+'/s'+str(seed).zfill(4)+'.mac', mode='w')
+	os.makedirs('./product/'+run_macro_name+'/'+output_dirname, exist_ok=True)
+	fout = open('./product/'+run_macro_name+'/'+output_dirname+'/s'+str(seed).zfill(4)+'.mac', mode='w')
 
 	for line in fin:
 		
@@ -32,8 +32,8 @@ def make_shell(seed):
 	loaded_shell_fname = job_maker_dir+'/loaded/shell.sh'
 	output_dirname = 'shell'
 	fin = open(loaded_shell_fname, mode='r')
-	os.makedirs('./made/'+run_macro_name+'/'+output_dirname, exist_ok=True)
-	foutname = './made/'+run_macro_name+'/'+output_dirname+'/s'+str(seed).zfill(4)+'.sh'
+	os.makedirs('./product/'+run_macro_name+'/'+output_dirname, exist_ok=True)
+	foutname = './product/'+run_macro_name+'/'+output_dirname+'/s'+str(seed).zfill(4)+'.sh'
 	fout = open(foutname, mode='w')
 
 	for line in fin:
@@ -44,16 +44,16 @@ def make_shell(seed):
 		elif 0 == line.find('    -o') :
 			fout.write('    -o ${workdir}/output'+str(seed).zfill(4)+'.root \n')
 		elif 0 == line.find('    -f') :
-			fout.write('    -f '+job_maker_dir+'/made/'+run_macro_name+'/macro/s'+str(seed).zfill(4)+'.mac \\\n')
+			fout.write('    -f '+job_maker_dir+'/product/'+run_macro_name+'/macro/s'+str(seed).zfill(4)+'.mac \\\n')
 		elif 0 == line.find('workdir=') :
 			fout.write('workdir='+workdir+'\n')
 		elif 0 == line.find('#SBATCH --job-name=') :
 			fout.write('#SBATCH --job-name=s'+str(seed).zfill(4)+'.sh\n')
 		elif 0 == line.find('#SBATCH --output=') :
-			#fout.write('#SBATCH --output='+job_maker_dir+'/made/'+run_macro_name+'/log/s'+str(seed).zfill(4)+'.o \n')
+			#fout.write('#SBATCH --output='+job_maker_dir+'/product/'+run_macro_name+'/log/s'+str(seed).zfill(4)+'.o \n')
 			fout.write('#SBATCH --output='+workdir+'/s'+str(seed).zfill(4)+'.o \n')
 		elif 0 == line.find('#SBATCH --error=') :
-			#fout.write('#SBATCH --error='+job_maker_dir+'/made/'+run_macro_name+'/log/s'+str(seed).zfill(4)+'.e \n')
+			#fout.write('#SBATCH --error='+job_maker_dir+'/product/'+run_macro_name+'/log/s'+str(seed).zfill(4)+'.e \n')
 			fout.write('#SBATCH --error='+workdir+'/s'+str(seed).zfill(4)+'.e \n')
 		elif 0 == line.find('	-i ${workdir}/output') :
 			fout.write('    -i ${workdir}/output'+str(seed).zfill(4)+' \\\n')
@@ -61,17 +61,17 @@ def make_shell(seed):
 			fout.write(line)
 	fout.close()
 	os.chmod(foutname, 0o755)
-	#os.makedirs('./made/'+run_macro_name+'/'+'log', exist_ok=True)
+	#os.makedirs('./product/'+run_macro_name+'/'+'log', exist_ok=True)
 
 
 def make_throw(NofBatchs):
 
-	foutname = './made/'+run_macro_name+'/throw.sh'
+	foutname = './product/'+run_macro_name+'/throw.sh'
 	fout = open(foutname, mode='w')
 
 	fout.write('#! /bin/bash \n')
 	for i in range(1,NofBatchs+1):
-		batchpath = job_maker_dir+'/made/'+run_macro_name+'/shell/s'+str(i).zfill(4)+'.sh'
+		batchpath = job_maker_dir+'/product/'+run_macro_name+'/shell/s'+str(i).zfill(4)+'.sh'
 		fout.write('sbatch '+batchpath+'\n')
 		fout.write('sleep 1s\n')
 
